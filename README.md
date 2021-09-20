86d1a2829d2f6b3be746a575e8ea2a098d5a063f
MohammedBakr44 <mohammed.mobile.tricks@gmail.com>
2021-09-20 14:29:45 +0200

    Added README and comments

---
+++ README.md
@@ -0,0 +1,27 @@
+# Arqam
+
+Arqam is a fictional education service that focuses on STEM subjects.
+
+# Demo
+
+TBD
+
+# Development
+
+The project is made to be responsive using flexbox and dynamics units, the logic is totally written with Vanilla JS using DOM functions with an account for legacy browsers ~~not 100%~~.
+
+# Installation
+
+I wanted to make it as lightweight as possible so no frameworks or libraries needs to be configured, just run `index.html` in your browser and it will work.
+
+# Resources and Attribution
+
+some of the resources I used:
+
+- [Running an event on multiple elements](https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/)
+
+- [Intersection Observer](https://usefulangle.com/post/118/javascript-intersection-observer)
+
+- [Getting screen resolution](https://www.tutorialrepublic.com/faq/how-to-detect-screen-resolution-with-javascript.php)
+- [Color Palette](https://colorhunt.co/palette/e8e8e8f0545430475e222831)
+- And a lof of [MDN](https://developer.mozilla.org/) pages.
--- js/app.js
+++ js/app.js
@@ -1,22 +1,16 @@
-let nav = document.querySelector("nav");
-let sections = document.getElementsByTagName("section");
+let nav = document.querySelector("nav"); // Selecting the nav element
+let sections = document.getElementsByTagName("section"); // get all the sections
 
+// Create nav bar links from the existing sections
 for (let i = 0; i < sections.length; i++) {
+  // create a new li
   let node = document.createElement("li");
+  // get the section id to use it in the link
   let section = sections[i].id;
+  // link the li to the section using `section` variable
   node.innerHTML = `<a href="#${section}">${section}</a>`;
+  // add everything to the ul
   nav.children[0].appendChild(node);
-}
-
-function getCurrent(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
 }
 
 //get all the links on the page containing # in the href (^= means starting with)
@@ -25,7 +19,6 @@ const links = document.querySelectorAll('a[href^="#"]');
 links.forEach((link) => {
   // getting the target element by choosing the element based on the ID in href value
   let scrollTarget = document.querySelector(`${link.getAttribute("href")}`);
-  console.log(scrollTarget);
   // for clicking on any of the links
   link.addEventListener("click", (e) => {
     // we need to prevent default behaviour which would be just jumping to the element without scrolling
@@ -35,20 +28,15 @@ links.forEach((link) => {
   });
 });
 
-// document.addEventListener('scroll',() => {
-//     [...sections].forEach(section => {
-//         if(getCurrent(section)) {
-//             console.log(section);
-//         }
-
-//     })
-// }, {passive: true})
-
+// Using intersectionObserver to add/remove the active class
 const observer = new IntersectionObserver(
   (entries) => {
+    // accepting entries so the observer works on many elements
     for (let entry of entries) {
-      entry.target.classList.remove("active");
+      // looping through all the elements
+      entry.target.classList.remove("active"); //remove the class from the rest of the elements
       if (entry.isIntersecting) {
+        // check if the element is intersecting with the screen, if so, add the class active
         entry.target.classList.add("active");
       }
     }
@@ -56,17 +44,19 @@ const observer = new IntersectionObserver(
   { threshold: [0.3] }
 );
 
+// Using spread operator to make an array out of sections, looping on it and observe every element
 [...sections].forEach((section) => observer.observe(section));
 
 // To top button
 const button = document.querySelector(".toTop");
 
 document.addEventListener("scroll", (e) => {
+  // Getting the scrolled amount to show the button, || is used to support more browsers
   document.body.scrollTop > 900 || document.documentElement.scrollTop > 900
     ? (button.style.display = "block")
     : (button.style.display = "none");
 });
-
+// Scroll back to the main element
 button.addEventListener("click", () =>
   document
     .getElementsByTagName("main")[0]
@@ -80,8 +70,7 @@ window.addEventListener("resize", () => {
     window.innerWidth ||
     document.documentElement.clientWidth ||
     document.body.clientWidth;
-  console.log(width);
-});
+}); // Update the width on windowResize
 
 document.getElementsByClassName("logo")[0].addEventListener("click", () => {
   if (width <= 768) {
@@ -89,4 +78,4 @@ document.getElementsByClassName("logo")[0].addEventListener("click", () => {
       .querySelectorAll("nav ul li:not(.logo)")
       .forEach((item) => item.classList.toggle("active-menu"));
   }
-});
+}); // When the logo is clicked, check for the width, if on mobile add the class active-menu(display: block) to all li elements except for the logo
